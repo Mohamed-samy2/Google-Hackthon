999751abd52e7625f102b3a4345a6debd419110c
Mohamed samy <92379704+Mohamed-samy2@users.noreply.github.com>
2025-11-19 09:23:40 +0200

    Remove API key from CarAgent initialization
    Removed sensitive API key from CarAgent.

--- Agents/CarAgent.py
+++ Agents/CarAgent.py
@@ -29,7 +29,7 @@ class CarAgent(Base):
         self.llm =ChatGoogleGenerativeAI(
             model="gemini-2.0-flash",
             temperature=0.4,
-            api_key='AIzaSyBMfI-quDrgCwNBFWologuKAkxgPXfiUkI',
+            api_key='',
             max_retries=2,
             )
         self.api_key="AIzaSyAcabkVCxzNzBK79AJvy3c-RajZxDqlOtU"
